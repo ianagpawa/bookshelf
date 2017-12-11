324c2e87cfc0334ef2eb75af999dd05c6149062e
Ian Agpawa <agpawaji@gmail.com>
2017-12-10 23:37:25 -0500

    feat: Start bookers reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+function () {
+    return [
+        {title: "Fear and Loathing in Las Vegas"},
+        {title: "Apathy and Other Small Victories"},
+        {title: "The Hitchhiker's Guide to the Galaxy"},
+        {title: "Winesburg, Ohio"},
+    ]
+}
