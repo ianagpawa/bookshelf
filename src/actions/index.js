10a145f14a8048d906eeae55551dd9b3fb077e18
Ian Agpawa <agpawaji@gmail.com>
2017-12-14 01:12:10 -0500

    feat: Add type and payload to selectBook ActionCreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book) {
-    console.log("A book has been selected: ", book.title);
+    // selectBook is an ActionCreator
+    return {
+        type: "BOOK_SELECTED",
+        payload: book
+    };
 }
