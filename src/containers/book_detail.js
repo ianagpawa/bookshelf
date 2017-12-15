c455e0b78129ba65ba6f45196bc13f4b72628962
Ian Agpawa <agpawaji@gmail.com>
2017-12-14 23:35:43 -0500

    refactor: Correct book detail

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -4,8 +4,7 @@ export default class BookDetail extends Component {
     render() {
         return (
             <div>
-                <BookList />
-                <BookDetial />
+                Book Detail
             </div>
         )
     }
