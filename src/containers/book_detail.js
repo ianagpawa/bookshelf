f3d43d407024e6068a90ecf3ea3f4879e707c172
Ian Agpawa <agpawaji@gmail.com>
2017-12-15 00:02:22 -0500

    refactor: Add connected component

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActinoCreators } from 'redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
         return (
             <div>
                 Book Detail
             </div>
-        )
+        );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
