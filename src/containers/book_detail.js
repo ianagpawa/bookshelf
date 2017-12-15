faa1581ce95867a45843261bd24f966447cf7331
Ian Agpawa <agpawaji@gmail.com>
2017-12-14 23:26:48 -0500

    feat: Add book detail container

---
+++ src/containers/book_detail.js
@@ -0,0 +1,12 @@
+import React, { Compoent } from 'react';
+
+export default class BookDetail extends Component {
+    render() {
+        return {
+            <div>
+                <BookList />
+                <BookDetial />
+            </div>
+        }
+    }
+}
